5e4a45f7d8ab1aec0e7cd400ccc9149254785db3
Polina Novoselova <polka.nov@mail.ru>
2024-06-20 19:33:29 +0200

    add slider swiper

--- feedbackSlider.js
+++ feedbackSlider.js
@@ -36,51 +36,74 @@
 //     return activeId === (slidesNodes.length - 1) ? 0: activeId + 1;
 // }
 
-const ACTIVE_SLIDE_CLASSNAME = 'gallery__slide_active';
+// const ACTIVE_SLIDE_CLASSNAME = 'gallery__slide_active';
 
-const slidesContainer = document.querySelector('.gallery__slides');
-let slidesNodes = Array.from(document.querySelectorAll('.gallery__slide'));
-const prevButtonNode = document.querySelector('.gallery__control-prev');
-const nextButtonNode = document.querySelector('.gallery__control-next');
-let activeIndex = 0;
+// const slidesContainer = document.querySelector('.gallery__slides');
+// let slidesNodes = Array.from(document.querySelectorAll('.gallery__slide'));
+// const prevButtonNode = document.querySelector('.gallery__control-prev');
+// const nextButtonNode = document.querySelector('.gallery__control-next');
+// let activeIndex = 0;
 
-init();
+// init();
 
-function init() {
-    setInitialPositions();
-    prevButtonNode.addEventListener('click', () => moveSlides('prev'));
-    nextButtonNode.addEventListener('click', () => moveSlides('next'));
-}
+// function init() {
+//     setInitialPositions();
+//     prevButtonNode.addEventListener('click', () => moveSlides('prev'));
+//     nextButtonNode.addEventListener('click', () => moveSlides('next'));
+// }
 
-function setInitialPositions() {
-    slidesNodes.forEach((slide, index) => {
-        slide.style.transition = 'none';
-        slide.style.transform = `translateX(${index * 100}%)`;
-    });
-}
+// function setInitialPositions() {
+//     slidesNodes.forEach((slide, index) => {
+//         slide.style.transition = 'none';
+//         slide.style.transform = `translateX(${index * 100}%)`;
+//     });
+// }
 
-function moveSlides(direction) {
-    slidesNodes.forEach(slide => slide.style.transition = 'transform 0.5s ease');
+// function moveSlides(direction) {
+//     slidesNodes.forEach(slide => slide.style.transition = 'transform 0.5s ease');
 
-    if (direction === 'next') {
-        slidesNodes.forEach((slide, index) => {
-            slide.style.transform = `translateX(${(index - 1) * 100}%)`;
-        });
-        setTimeout(() => {
-            const firstSlide = slidesNodes.shift();
-            slidesNodes.push(firstSlide);
-            slidesContainer.appendChild(firstSlide);
-            setInitialPositions();
-        }, 500); // Duration of the CSS transition
-    } else {
-        slidesNodes.forEach((slide, index) => {
-            slide.style.transform = `translateX(${(index + 1) * 100}%)`;
-        });
-        setTimeout(() => {
-            const lastSlide = slidesNodes.pop();
-            slidesNodes.unshift(lastSlide);
-            slidesContainer.insertBefore(lastSlide, slidesNodes[0]);
-            setInitialPositions();
-        }, 500); // Duration of the CSS transition
-    }
-}
+//     if (direction === 'next') {
+//         slidesNodes.forEach((slide, index) => {
+//             slide.style.transform = `translateX(${(index - 1) * 100}%)`;
+//         });
+//         setTimeout(() => {
+//             const firstSlide = slidesNodes.shift();
+//             slidesNodes.push(firstSlide);
+//             slidesContainer.appendChild(firstSlide);
+//             setInitialPositions();
+//         }, 500); // Duration of the CSS transition
+//     } else {
+//         slidesNodes.forEach((slide, index) => {
+//             slide.style.transform = `translateX(${(index + 1) * 100}%)`;
+//         });
+//         setTimeout(() => {
+//             const lastSlide = slidesNodes.pop();
+//             slidesNodes.unshift(lastSlide);
+//             slidesContainer.insertBefore(lastSlide, slidesNodes[0]);
+//             setInitialPositions();
+//         }, 500); // Duration of the CSS transition
+//     }
+// }
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
